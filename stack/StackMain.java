f532a253bd4bec9213e3cc7f09b4914bdd538380
amod1811 <ammch101@gmail.com>
2022-05-03 00:42:33 +0530

    Added:program to Add the Element in Stack

---
+++ stack/StackMain.java
@@ -0,0 +1,19 @@
+package com.bl.stack;
+/*
+ * Author:Amod chaudhari
+ * perform the push pop operation To add and remove Element
+ */
+import java.util.Scanner;
+
+import com.bl.stack.StackImplementation;
+
+public class StackMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Satck program");
+		System.out.println();
+		StackImplementation stackImplementation = new StackImplementation();
+		stackImplementation.userMenu();
+	}
+	
+}
