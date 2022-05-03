943adf6fb1527318d5c02473ae9306e0f24017dd
amod1811 <ammch101@gmail.com>
2022-05-03 17:53:30 +0530

    Added queue program

--- queue/QueueMain.java
+++ queue/QueueMain.java
@@ -1,6 +1,8 @@
 package com.bl.queue;
+
 /*
- * perform the operation on queue to add and remove element using EnQueue and DeQueue function
+ * Author : Amod Chaudhari
+ * perform the operation on queue to add and remove element using InQueue and DeQueue function
  */
 import java.util.Scanner;
 
